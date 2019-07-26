d08ce18f60be7f1ec5f4effce02606f335d94c61
Toggor <toggor@gmail.com>
2019-07-26 12:30:12 +0300

    Overall structure 1 trying recyclerview

--- app/src/main/java/com/example/metersinspector/MainActivity.java
+++ app/src/main/java/com/example/metersinspector/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.metersinspector;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.metersinspector.MyRecyclerViewAdapter;
+import com.example.metersinspector.R;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {   // here we list EventReports
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
-    ArrayList<EventReport> eventReports = new ArrayList<EventReport>();
+    MyRecyclerViewAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { //we should list all the EventReports that are present
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
 
 
     //TODO: - create a new EventReport button
