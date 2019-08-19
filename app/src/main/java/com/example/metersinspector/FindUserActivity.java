85c86d35ec292369416cef4f36346c536a0eae9b
Toggor <toggor@gmail.com>
2019-08-19 14:32:53 +0300

    Creating recyclerView prepared, forward to populating the view with contacts

--- app/src/main/java/com/example/metersinspector/FindUserActivity.java
+++ app/src/main/java/com/example/metersinspector/FindUserActivity.java
@@ -1,9 +1,13 @@
 package com.example.metersinspector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class FindUserActivity extends AppCompatActivity {
 
@@ -11,9 +15,23 @@ public class FindUserActivity extends AppCompatActivity {
     private RecyclerView.Adapter mUserListAdapter;
     private RecyclerView.LayoutManager mUserListLayoutManager;
 
+    ArrayList<UserObject> userList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
+        userList = new ArrayList<UserObject>();
+        initializeRecyclerView();
+    }
+
+    private void initializeRecyclerView() {
+        mUserList = findViewById(R.id.userList);
+        mUserList.setNestedScrollingEnabled(false);
+        mUserList.setHasFixedSize(false);
+        mUserListLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.VERTICAL, false);
+        mUserList.setLayoutManager(mUserListLayoutManager);
+        mUserListAdapter = new UserListAdapter(userList);
+        mUserList.setAdapter(mUserListAdapter);
     }
 }
---
+++ app/src/main/java/com/example/metersinspector/UserObject.java
@@ -0,0 +1,18 @@
+package com.example.metersinspector;
+
+class UserObject {
+    private String name, phone;
+
+    public UserObject(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
